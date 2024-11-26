7b758dc7c624a383d9acfaad9bc8164f81406c58
Priscila Romero <priscillaromero95@gmail.com>
2024-11-26 17:45:20 -0300

    Project completed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://Priscila1995.github.io/weather-app',
+  base: 'https://priscila1995.github.io/weather-app',
 })
